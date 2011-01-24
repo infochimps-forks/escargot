7710d1624a8e45322e5fe3235697dba8c966d9bc
Dhruv Bansal <dhruv@ph.utexas.edu>
2011-01-24 17:21:38 -0600

    don't declare $elastic_search_client in init.rb.

--- rails/init.rb
+++ rails/init.rb
@@ -11,11 +11,3 @@ end
 ElasticSearch::Client.class_eval do
   include Escargot::AdminIndexVersions
 end
-
-unless File.exists?(Rails.root + "/config/elasticsearch.yml")
-  Rails.logger.warn "No config/elastic_search.yaml file found, connecting to localhost:9200"
-  $elastic_search_client = ElasticSearch.new("localhost:9200")
-else
-  config = YAML.load_file(RAILS_ROOT + "/config/elasticsearch.yml")
-  $elastic_search_client = ElasticSearch.new(config["host"] + ":" + config["port"].to_s, :timeout => 20)
-end
