e06a0c23387e7c51edc183cc1cab31d333b95a9d
Dhruv Bansal <dhruv@ph.utexas.edu>
2011-06-28 15:10:44 -0500

    moved init.rb to top-level

--- init.rb
+++ init.rb
@@ -1,1 +1,13 @@
-# Include hook code here
+require 'escargot'
+
+ActiveRecord::Base.class_eval do
+  include Escargot::ActiveRecordExtensions
+end
+
+ElasticSearch::Api::Hit.class_eval do
+  include Escargot::HitExtensions
+end
+
+ElasticSearch::Client.class_eval do
+  include Escargot::AdminIndexVersions
+end
